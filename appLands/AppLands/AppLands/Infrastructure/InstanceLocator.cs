0f88bd9f9f392205af1fa93e017c94043dcab086
emilio barrera <barrera_emilio@yahoo.es>
2018-02-19 23:54:11 +0000

    App Lands
    AppLands vs 2017  with xamarin

---
+++ appLands/AppLands/AppLands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AppLands.ViewModels;
+
+namespace AppLands.Infrastructure
+{
+   public class InstanceLocator
+    {
+        #region Properties
+
+        public MainViewModel Main
+        {
+            get; set; 
+
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+
+        #endregion
+    }
+}
---
+++ appLands/AppLands/AppLands/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppLands.ViewModels
+{
+    public class MainViewModel
+    {
+    }
+}
