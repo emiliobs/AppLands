6db4922f46566d09ef7ee187905c091d2e2114d2
emilio barrera <barrera_emilio@yahoo.es>
2018-02-20 02:03:34 +0000

    Developing all the part of the lolginViewModel

---
+++ appLands/AppLands/AppLands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,50 @@
+﻿ 
+namespace AppLands.ViewModels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Windows.Input;
+    public class LoginViewModel
+    {
+        #region Atributtes
+
+        #endregion
+
+        #region Properties
+
+        public string Email { get; }
+
+        public string Password
+        {
+            get;
+        }
+
+        public bool IsRunning
+        {
+            get;
+        }
+
+        public bool IsRemember
+        {
+            get;
+        }
+
+        #endregion
+
+        #region Commamds
+        public ICommand LoginCommand { get; }
+        #endregion
+
+        #region Contructor
+        public LoginViewModel()
+        {
+            this.IsRemember = true;
+        }
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
--- appLands/AppLands/AppLands/ViewModels/MainViewModel.cs
+++ appLands/AppLands/AppLands/ViewModels/MainViewModel.cs
@@ -6,5 +6,19 @@ namespace AppLands.ViewModels
 {
     public class MainViewModel
     {
+        #region ViewModels
+
+        public LoginViewModel Login { get; set; }
+
+        #endregion
+
+        #region Contructor
+
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+
+        #endregion
     }
 }
