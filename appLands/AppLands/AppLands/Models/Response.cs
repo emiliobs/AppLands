fa5166e4e322dd3b9f71366da85e60971083b256
emilio barrera <barrera_emilio@yahoo.es>
2018-02-20 14:32:07 +0000

    apiService and Tokenresponse

---
+++ appLands/AppLands/AppLands/Models/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppLands.Models
+{
+    public class Response
+    {
+        public bool IsSuccess { get; set; }
+        public String Message { get; set; }
+        public Object Result { get; set; }
+    }
+}
