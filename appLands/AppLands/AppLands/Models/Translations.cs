37b52f7816597bfe80cd0238dfc249ae6f2b0ad8
emilio barrera <barrera_emilio@yahoo.es>
2018-03-01 13:38:39 +0000

    translation is done.!

--- appLands/AppLands/AppLands/Models/Translations.cs
+++ appLands/AppLands/AppLands/Models/Translations.cs
@@ -35,7 +35,7 @@ namespace AppLands.Models
         public string Croatian { get; set; }
 
         [JsonProperty(PropertyName = "fa")]
-        public string Danish { get; set; }
+        public string Persian { get; set; }
 
 
     }
